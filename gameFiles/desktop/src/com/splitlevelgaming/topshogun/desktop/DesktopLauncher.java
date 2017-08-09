d899ac4d2dd767af66917b4395f37f6d49dc5b46
splitlevelgaming <splitlevelgaming@gmail.com>
2017-08-08 19:40:25 -0500

    Re-added boiler plate

---
+++ gameFiles/desktop/src/com/splitlevelgaming/topshogun/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.splitlevelgaming.topshogun.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.splitlevelgaming.topshogun.MainGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainGame(), config);
+	}
+}
