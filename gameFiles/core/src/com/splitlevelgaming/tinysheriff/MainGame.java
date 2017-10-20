7d6804ead79cef430ad9c72ce7dfacd638b1b046
SplitLevelGaming <splitlevelgaming@gmail.com>
2017-10-19 19:36:19 -0700

    Update MainGame.java

--- gameFiles/core/src/com/splitlevelgaming/tinysheriff/MainGame.java
+++ gameFiles/core/src/com/splitlevelgaming/tinysheriff/MainGame.java
@@ -57,7 +57,7 @@ public class MainGame extends ApplicationAdapter {
 			controllers[i].refresh();
 		}
 		//Tell stage to begin the step
-		activeStage.activate(pen, controllers);
+		activeStage.activate(pen);
 		//The following lines are here for testing purposes. They should not be uncommented in any PR.
 		//font.draw(batch, screenWidth + ", " + screenHeight, 0, 15);
 		//font.draw(batch, Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory() + "", 0, 30);
